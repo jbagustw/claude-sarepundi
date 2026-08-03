dffc1e868596b16b128f425b4c34736b2c62a8e9
jbagustw <jbagustw@gmail.com>
2026-08-03 22:38:47 +0700

    fix favicon

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+const { data: siteSettings } = await useSiteSettings()
+const faviconHref = computed(() => siteSettings.value?.favicon_url || '/favicon.ico')
+
+useHead({
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: faviconHref },
+  ],
+})
+</script>
+
 <template>
   <div>
     <NuxtLayout>
